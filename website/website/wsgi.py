8701fae33b615222e465dab47342dd29b74cbe54
Ayush Jain <ayushjai@iitg.ac.in>
2019-10-26 22:33:46 +0530

    Update wsgi.py

--- website/website/wsgi.py
+++ website/website/wsgi.py
@@ -6,11 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
