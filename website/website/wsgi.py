79fa35c4216baa7438c0b44834b0f81757c71b70
Ayush Jain <ayushjai@iitg.ac.in>
2019-10-27 01:18:48 +0530

    Update wsgi.py

--- website/website/wsgi.py
+++ website/website/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for website project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
 import os
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'website.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
