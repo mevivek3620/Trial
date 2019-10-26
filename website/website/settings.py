83fda5fce2d2bee74d88d67075988233e51014cc
AJ-54 <ayushjai@iitg.ac.in>
2019-10-26 23:46:06 +0530

    Trying to Deploy

--- website/website/settings.py
+++ website/website/settings.py
@@ -123,4 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
 django_heroku.settings(locals())
--- website/website/urls.py
+++ website/website/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include("core.urls")),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
