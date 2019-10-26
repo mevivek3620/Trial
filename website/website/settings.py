63cf14aff6b3fabf967d84320aacb656a33dda95
Ayush Jain <ayushjai@iitg.ac.in>
2019-10-27 02:29:19 +0530

    Update settings.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'core',
+    'website.core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
